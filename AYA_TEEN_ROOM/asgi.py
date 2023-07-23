2728c282a3b1277a3ed817c9ac2c9d50028d29c2
sarahliz24 <sarahliz24@hotmail.com>
2023-07-23 11:27:17 +0000

    Install django & supporting libraries

---
+++ AYA_TEEN_ROOM/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for AYA_TEEN_ROOM project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AYA_TEEN_ROOM.settings')
+
+application = get_asgi_application()
---
+++ AYA_TEEN_ROOM/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for AYA_TEEN_ROOM project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AYA_TEEN_ROOM.settings')
+
+application = get_wsgi_application()
