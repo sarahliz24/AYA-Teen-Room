a1d11aef9d2ee8bd4c00d08e6da310eda0d52064
sarahliz24 <sarahliz24@hotmail.com>
2023-08-09 09:11:20 +0000

    Add basic structure to home page

--- AYA_TEEN_ROOM/urls.py
+++ AYA_TEEN_ROOM/urls.py
@@ -16,6 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from PAGES.views import (
+    home_view,
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home_view, name="home"),
 ]
