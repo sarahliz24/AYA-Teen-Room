dc39a916f86c60bda97091778fbc867de4123668
sarahliz24 <sarahliz24@hotmail.com>
2023-08-12 14:51:39 +0000

    Add view & template to account app for login

--- account/forms.py
+++ account/forms.py
@@ -1,5 +1,5 @@
 from django import forms
 
 class FormLoggingIn(forms.Form):
-    username = forms.Charfield()
-    password = forms.Charfield(widget=forms.PasswordInput)
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
