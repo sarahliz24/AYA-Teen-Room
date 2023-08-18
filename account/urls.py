bc2284a8f0a444d8e866649ba74d1372fa92d816
sarahliz24 <sarahliz24@hotmail.com>
2023-08-18 16:48:56 +0000

    Add change password function for logged in user

--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views
+
 # from .views import signup
 
 urlpatterns = [
@@ -15,4 +16,7 @@ urlpatterns = [
     path('edit/', views.ProfileEdit, name='edit'),
 
     # path('signup/', signup.as_view(), name='signup'),
+    path('password-change/', auth_views.PasswordChangeView.as_view(),
+          name='password_change'),
+    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
 ]
