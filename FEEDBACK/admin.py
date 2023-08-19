3ffb9152f4022af4158120d8cf92a6f0194b504f
sarahliz24 <sarahliz24@hotmail.com>
2023-08-19 15:24:31 +0000

    Add custom admin class for feedback app

--- FEEDBACK/admin.py
+++ FEEDBACK/admin.py
@@ -1,4 +1,14 @@
 from django.contrib import admin
 from .models import Feedback
 
-admin.site.register(Feedback)
+# admin.site.register(Feedback)
+
+@admin.register(Feedback)
+class FeedbackAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug', 'author', 'room', 'feedback_approval']
+    list_filter = ['author',  'room', 'feedback_approval', 'feedback_made']
+    search_fields = ['title', 'content']
+    prepopulated_fields = {'slug': ('title',)} # auto-fills slug based on title
+    raw_id_fields = ['author']  # widget to search for author in admin view
+    date_hierachy = 'show_feedback'
+    ordering = ['feedback_approval', 'show_feedback']
