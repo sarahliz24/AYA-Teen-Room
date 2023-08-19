5493af3db6484d5d10a2a2c7dadbb5f5d1d7976f
sarahliz24 <sarahliz24@hotmail.com>
2023-08-19 11:39:03 +0000

    Add help text & validators to TeenUserProfile model

--- account/models.py
+++ account/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 # from django.contrib.auth.models import AbstractUser
 from django.conf import settings
+from django.core.validators import MinLengthValidator, int_list_validator
 
 
 """ class TeenUser(AbstractUser):
@@ -17,11 +18,15 @@ from django.conf import settings
 class TeenUserProfile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL,
                                 on_delete=models.CASCADE, related_name='profile')
-    date_of_birth = models.DateField(blank=True, null=True)
+    date_of_birth = models.DateField(blank=False, null=True,
+                                     help_text='Format: 1900/12/31')
     # blank = False forces medical ID to be completed on form, but allows empty value in DB table (so no errors when profile view created but not yet filled out)
-    medical_id = models.IntegerField(blank=False, null=True,
+    # https://stackoverflow.com/questions/57131043/how-to-make-integer-field-fixed-to-ten-numbers-output-in-models
+    medical_id = models.CharField(blank=False, null=True,
                                      unique=True,
-                                     help_text='This is used for safeguarding compliance only')
+                                     help_text='Required: this is a 7 digit number found on your wristband',
+                                     max_length=7, 
+                                     validators=[int_list_validator(sep=''),MinLengthValidator(7),])
     fname = models.CharField(max_length=20, blank=True, null=True)
 
     def __str__(self):
