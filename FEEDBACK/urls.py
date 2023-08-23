bbc622e3a170a80940e43047083dec321127adb6
sarahliz24 <sarahliz24@hotmail.com>
2023-08-23 14:39:30 +0000

    "Add path for post reply"

--- FEEDBACK/urls.py
+++ FEEDBACK/urls.py
@@ -5,5 +5,8 @@ app_name = 'FEEDBACK'
 
 urlpatterns = [
     path('', views.feedback_list, name='feedback_list'),
+    # path('', views.FeedbackListView.as_view(), name='feedback_list'),
     path('<int:id>/', views.feedback_detail, name='feedback_detail'),
+    path('<int:feedback_id>/reply/', views.post_reply, name='post_reply'),
+    # path('feedback-detail/', views.feedback_detail, name='feedback_detail'),
 ]
