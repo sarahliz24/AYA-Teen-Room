8c602ac122d434464a469b7317b9785d743807ab
sarahliz24 <sarahliz24@hotmail.com>
2023-08-23 18:01:48 +0000

    Add view & urls for feedback form submission

--- FEEDBACK/urls.py
+++ FEEDBACK/urls.py
@@ -5,6 +5,7 @@ app_name = 'FEEDBACK'
 
 urlpatterns = [
     path('', views.feedback_list, name='feedback_list'),
+    path('feedback_submission/', views.feedback_submission, name='feedback_submission'),
     # path('', views.FeedbackListView.as_view(), name='feedback_list'),
     path('<int:id>/', views.feedback_detail, name='feedback_detail'),
     path('<int:feedback_id>/reply/', views.post_reply, name='post_reply'),
--- FEEDBACK/views.py
+++ FEEDBACK/views.py
@@ -1,14 +1,67 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import Http404
 # from django.views import generic
+from django.contrib import messages
 from .models import Feedback, FeedbackReply
-from .forms import ReplyForm
+from .forms import FeedbackSubmission, ReplyForm
 from django.views.decorators.http import require_POST
 from django.views.generic import ListView
 
 
 def home_view(request):
     return render(request, "FEEDBACK/home.html", {})
+
+
+def feedback_submission(request):
+    '''
+    form = FeedbackSubmission(data=request.POST)
+    if form.is_valid():
+        feedback_form.instance.author = request.user.username
+        feedback = feedback_form.save(commit=False)
+        # feedback.post = post
+        feedback.save()
+    else:
+       form = FeedbackSubmission() 
+    return render(request, 'FEEDBACK/feedback/feedback_submission.html',)
+    '''
+    form = FeedbackSubmission()
+    if request.method == 'POST':
+        form = FeedbackSubmission(request.POST)
+        # feedback_submission = FeedbackSubmission(instance=request.user, data=request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Your feedback has been submitted & is awaiting approval")
+            return render(request, 'FEEDBACK/feedback/feedback_list.html')
+        else:
+            messages.error(request, 'Oops, something went wrong!')
+    else:
+        form = FeedbackSubmission(instance=request.user)
+    return render(request, 'FEEDBACK/feedback/feedback_submission.html',
+                  {'form': form, })
+
+
+'''
+    if request.method == 'POST':
+        signup_form = EditUser(instance=request.user, data=request.POST)
+        teen_user_profile = EditTeenUserProfile(
+                                        instance=request.user.profile,
+                                        data=request.POST,
+                                        files=request.FILES)
+        if signup_form.is_valid() and teen_user_profile.is_valid():
+            signup_form.save()
+            teen_user_profile.save()
+            messages.success(request, "Update of your details successful")
+            return render(request, 'feedback/feedback_list.html')
+        else:
+            messages.error(request, 'Oops, something went wrong!')
+    else:
+        signup_form = EditUser(instance=request.user)
+        teen_user_profile = EditTeenUserProfile(
+                                        instance=request.user.profile)
+    return render(request, 'registration/edit.html',
+                  {'signup_form': signup_form,
+                   'teen_user_profile': teen_user_profile})
+'''
 
 
 def feedback_list(request):
