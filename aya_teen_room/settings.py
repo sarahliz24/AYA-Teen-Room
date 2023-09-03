bc23dd1fa12537a851a6f03509e245c69fa740c6
sarahliz24 <sarahliz24@hotmail.com>
2023-09-03 03:45:18 +0000

    Fix: replace delete_feedback template

--- aya_teen_room/settings.py
+++ aya_teen_room/settings.py
@@ -32,7 +32,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'aya-teen-room-1764987b8851.herokuapp.com',
