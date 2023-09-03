95980d69b9d43229d36406a9a7b2c4177cc49011
sarahliz24 <sarahliz24@hotmail.com>
2023-09-03 02:22:44 +0000

    set debug to false

--- aya_teen_room/settings.py
+++ aya_teen_room/settings.py
@@ -32,7 +32,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'aya-teen-room-1764987b8851.herokuapp.com',
